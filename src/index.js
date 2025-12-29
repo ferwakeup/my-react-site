f5db91962e90a0c4998919c544139cae6b3c8b0f
Fernando Martín <fernandomartin@MacBook-Air-de-Fernando.local>
2025-12-30 00:40:01 +0100

    Fixed Tailwind v4 import order

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // This assumes your main code is in App.js
 import './App.css';
+import App from './App'; // This assumes your main code is in App.js
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
