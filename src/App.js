b9a84bd13132ec93df84c97cfe32751e289deab3
Fernando Martín <fernandomartin@MacBook-Air-de-Fernando.local>
2025-12-29 23:52:56 +0100

    test build

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Vercel is Working!</h1>
+      <p>If you see this, the problem is in the previous Gemini code.</p>
+    </div>
+  );
+}
+
+export default App;
